6c81d7ae0269190be72f08a301e8ab5888fe02e8
Niroasd <123634396+Niroasd@users.noreply.github.com>
2024-12-12 22:35:20 +0200

    Adding profile base

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import hello2 from '../assets/hello2.jpg'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <>
+      <div id='home' className='px-16 flex min-h-screen w-full items-center justify-center py-28 md:px-32'>
+        <div className='flex flex-col items-center justify-center gap-10 text-white'>
+          <div>
+            <img src={hello2} className='w-[300px] shadow-xl shadow-red-200 rounded-full'></img>
+            <p>hey👋</p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
